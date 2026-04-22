6a6e644c67dc8114a0c19e48bf1e1c02065a9dec
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2026-04-22 21:08:27 +0530

    chore: added sm_89 sass and ptx dump for 01_vector_add

---
+++ 01_vector_add/sm_89/01_vector_add.cu
@@ -0,0 +1,28 @@
+// sass-king step 01: vector add
+// Goal: observe the minimal SASS skeleton
+// Compile: nvcc -arch=sm_120 -o 01_vector_add 01_vector_add.cu
+// Dump:    cuobjdump --dump-sass 01_vector_add
+
+__global__ void vector_add(const float* a, const float* b, float* c, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    const int n = 1024;
+    const int bytes = n * sizeof(float);
+
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+
+    vector_add<<<(n + 255) / 256, 256>>>(d_a, d_b, d_c, n);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    return 0;
+}
